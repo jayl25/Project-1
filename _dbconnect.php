3b3ce7a4afe74b3158b695bb8c445ba1eeb4606a
jayl25 <113488329+jayl25@users.noreply.github.com>
2022-11-03 20:38:51 -0400

    Add files via upload

---
+++ _dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+ 
+    $username = "root";
+    $password ="";
+    $server= "localhost";
+    $conn = mysqli_connect($server, $username, $password);
+
+    if(!$conn){
+        die("Could not connect to server " . mysqli_connect_error());
+    }
+
+    $sql = "CREATE DATABASE IF NOT EXISTS `demo-registration`";
+    mysqli_query($conn, $sql);
+
+    $sql = "CREATE TABLE IF NOT EXISTS `demo-registration`.`credentials` (`id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) NOT NULL , `password` VARCHAR(255) NOT NULL , `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    mysqli_query($conn, $sql);
+    
+    $sql = "CREATE TABLE IF NOT EXISTS `demo-registration`.`user_points` (`id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(16) NOT NULL , `score` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    mysqli_query($conn, $sql);
+?>
